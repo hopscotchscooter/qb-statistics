e6787ff95ae48d4f4d7127d7a518147aa08a2adc
david <dvlewicki@gmail.com>
2018-01-02 15:53:28 -0500

    Add in still images of project for readme

--- src/app/services/stats-service.ts
+++ src/app/services/stats-service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { BehaviorSubject } from 'rxjs';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 
